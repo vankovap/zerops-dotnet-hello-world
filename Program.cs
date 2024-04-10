0f33ae64bf9df6deebca29fa70b641f2d32519cf
vankovap <petravankova.pv@gmail.com>
2024-04-10 15:38:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,5 @@ using Microsoft.AspNetCore.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-app.Urls.Add("http://0.0.0.0:5000");
 app.MapGet("/", () => Results.Ok("Hello World!"));
 app.Run();
