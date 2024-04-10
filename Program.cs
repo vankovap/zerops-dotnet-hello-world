cd6f7563434d7f3a4965a4989310eb34e43f3d0e
vankovap <petravankova.pv@gmail.com>
2024-04-10 15:24:34 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,5 +4,5 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 app.Urls.Add("http://0.0.0.0:5000");
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Ok("Hello World!"));
 app.Run();
